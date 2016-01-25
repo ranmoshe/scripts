f23528da31176d92e254e5b0ba67fd7312a83b40
Ran Moshe <rani.moshe@gmail.com>
2016-01-25 11:33:28 +0200

    complete environment - for now...

--- django_angular_create.sh
+++ django_angular_create.sh
@@ -5,9 +5,20 @@
 source django_angular_settings.sh
 cd $parent_dir
 command -v pyvenv-3.4 >/dev/null 2>&1 || { echo >&2 "I require pyvenv-3.4 but it's not installed (sudo apt-get install python3.4-venv).  Aborting."; exit 1; }
-pyvenv-3.4 --clear --without-pip $venv_dir
+pyvenv-3.4 --clear $venv_dir
 cd $venv_dir
 echo 'now in'; pwd 
 source bin/activate
-#here I need to figure out how to install pip
-pip install djangorestframework
+pip install django
+pip install python-mimeparse
+pip install python-dateutil
+pip install django-tastypie
+django-admin startproject $mysite
+#####################################################
+# Make sure 'tastypie' is added to settings.py
+#####################################################
+response='n'
+while ! [[ $response =~ ^Y$ ]] 
+do
+    read -r -p "You need to add 'tastypie' to $mysite/$mysite/settings.py. Please hit 'Y' when you have.  " response
+done
